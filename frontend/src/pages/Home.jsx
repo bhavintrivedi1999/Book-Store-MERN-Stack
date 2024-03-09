0234e463ba607aa6c6933b200f72290b6f2afe89
BHAVIN <trivedibhavin1999@gmail.com>
2024-03-10 00:40:58 +0530

    changing url of src/pages files as per vercel url

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('https://book-store-mern-stack-api-lyart.vercel.app/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
